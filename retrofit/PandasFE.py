a658ed9fa6a98acd84ebfcad5313df7c44c6af93
Sean Benner <sean_benner@swifttrans.com>
2021-09-14 23:45:36 -0700

    restructured files

---
+++ retrofit/PandasFE.py
@@ -0,0 +1,2 @@
+import pandas as pd
+from FeatureEngineering import FeatureEngineering
---
+++ retrofit/PolarsFE.py
@@ -0,0 +1,7 @@
+import polars as pl
+from retrofit.FeatureEngineering import FeatureEngineering
+
+
+class FE(FeatureEngineering):
+    def __init__(self) -> None:
+        super().__init__()
