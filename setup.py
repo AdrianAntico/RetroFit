df2c9845852947bccea7b4b4cd62db80e84c7244
Adrian Antico <adrianantico@gmail.com>
2021-09-16 10:45:22 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     license="MIT",
     packages=find_packages(),
     include_package_data=True,
+    package_data={'': ['datasets/*.csv']},
     install_requires=required,
     classifiers=[
         "License :: OSI Approved :: MIT License",
