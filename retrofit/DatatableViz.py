7e35974ef489e4775b8e31b736a477cf278d13d2
Adrian Antico <adrianantico@gmail.com>
2021-11-20 13:36:10 -0700

    Updates

--- retrofit/DatatableViz.py
+++ retrofit/DatatableViz.py
@@ -20,7 +20,7 @@ def ScatterPlot(data=None,
                 SizeVar=None,
                 SymbolVar=None,
                 HoverStatsVar=None,
-                Markers=False):
+                MarginalPlots = False):
     """
     # Goal:
     Automatically generate scatterplots from datatable data
@@ -39,6 +39,7 @@ def ScatterPlot(data=None,
     ColorVar:      String, column name of the variable to use for the color coding of dots
     SizeVar:       String, column name of the variable to use for the sizing of dots
     HoverStatsVar: String, column name of the variable to use for mouse hovering stats
+    MarginalPlots: Logical, True to add histogram plots to both axis
     """
     
     # Ensure datatable
@@ -89,10 +90,13 @@ def ScatterPlot(data=None,
     data_pandas = data[:, Keep].to_pandas()
     
     # Build plot object
-    fig = px.scatter(data_pandas, x=XVar, y=YVar, color=ColorVar, size=SizeVar, hover_name=HoverStatsVar, facet_col=FacetColVar, facet_row=FacetRowVar)
-    
+    if MarginalPlots:
+        fig = px.scatter(data_pandas, x=XVar, y=YVar, color=ColorVar, size=SizeVar, hover_name=HoverStatsVar, facet_col=FacetColVar, facet_row=FacetRowVar, marginal_x='histogram', marginal_y='histogram'))
+    else:
+        fig = px.scatter(data_pandas, x=XVar, y=YVar, color=ColorVar, size=SizeVar, hover_name=HoverStatsVar, facet_col=FacetColVar, facet_row=FacetRowVar))
+
     # Generate plot
     fig.show()
-    
+
     # Return plot object
     return fig
--- tests/DataViz_QA.py
+++ tests/DataViz_QA.py
@@ -10,7 +10,22 @@ from retrofit import DatatableViz as dtv
 FilePath = pkg_resources.resource_filename('retrofit', 'datasets/BenchmarkData.csv')
 data = dt.fread(FilePath)
 
-dtv.ScatterPlot(
+# No marginal plots
+x = dtv.ScatterPlot(
+  data = data, 
+  N = 10000, 
+  XVar = 'XREGS1', 
+  YVar = 'Leads', 
+  FacetColVar = 'MarketingSegments',  
+  FacetRowVar = 'MarketingSegments2',
+  ColorVar = 'Leads',
+  SizeVar = 'Leads', 
+  SymbolVar = None,
+  HoverStatsVar = None,
+  MarginalPlots = False)
+
+# With marginal plots
+x = dtv.ScatterPlot(
   data = data, 
   N = 10000, 
   XVar = 'XREGS1', 
@@ -21,5 +36,4 @@ dtv.ScatterPlot(
   SizeVar = 'Leads', 
   SymbolVar = None,
   HoverStatsVar = None, 
-  Markers = None)
-  
+  MarginalPlots = True)
